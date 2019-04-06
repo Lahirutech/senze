3ad65154be4dcf85cadaa9af428e83f8034f4d77
lahirutech <rmllbrathnayaka@gmail.com>
2019-04-06 16:46:27 +0530

    Adding the basic components like landing page and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import  {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
+
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My react App</h1>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Route exact path='/' Component={Landing}/>
+            <Footer/>
+
       </div>
+      </Router>
     );
   }
 }
