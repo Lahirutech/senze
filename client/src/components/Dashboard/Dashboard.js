9eb74f5b121c09324c63cf5aa63cd229baeb9968
lahirutech <rmllbrathnayaka@gmail.com>
2019-04-07 10:38:40 +0530

    Adding the dashboard component

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+export default class dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Dashboard</h1>
+      </div>
+    )
+  }
+}
--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 
+
+
+
  class Landing extends Component {
   render() {
     return (
