5808dd20dc469f62ff20c188b5baa9a1e5c7a2d5
lahirutech <rmllbrathnayaka@gmail.com>
2019-04-07 10:15:42 +0530

    adding jwt decode and the dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import  {BrowserRouter as Router, Route} from 'react-router-dom';
 import{Provider} from 'react-redux';
 import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utills/setAuthToken';
+import {setCurrentUser} from './actions/authActions';
 
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+
 
 
 import './App.css';
 
+//check for token
+if(localStorage.jwtToken){
+  //set aucthtoken header oauth
+  setAuthToken(localStorage.jwtToken);
+  //decodoing token
+  const decoded=jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticvated
+  store.dispatch(setCurrentUser(decoded)); 
 
+
+}
 
 class App extends Component {
   render() {
@@ -25,6 +40,7 @@ class App extends Component {
            <div className="container">
              <Route exact path='/register/new' component={Register} />
              <Route exact path='/login' component={Login} />
+             <Route exact path='/dashboard' component={Dashboard}/>
            </div>
            <Footer/>
 
