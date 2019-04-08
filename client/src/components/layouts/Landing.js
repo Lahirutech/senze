c4fd755039ae856f0bead68eba65bcc7a6e07dd0
lahirutech <rmllbrathnayaka@gmail.com>
2019-04-08 18:04:13 +0530

    Updating the landing page

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -12,9 +12,9 @@ import {Link} from 'react-router-dom';
       <div className="container">
         <div className="row">
           <div className="col-md-12 text-center">
-            <h1 className="display-3 mb-4">Developer Connector
+            <h1 className="display-3 mb-4">Senz Dashboard
             </h1>
-            <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+            <p className="lead"> Create a Profie and Join with the Senz Platform</p>
             <hr />
             <Link to="/register/new" className="btn btn-lg btn-info mr-2">Sign Up</Link>
             <Link to="/login" className="btn btn-lg btn-light">Login</Link>
